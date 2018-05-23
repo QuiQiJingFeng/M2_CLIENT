b3d92226683ffa617cc593d9430a11cedf5fca59
yezhihui <15201362584@163.com>
2018-05-23 16:57:18 +0800

    麻将的游戏设置

--- src/app/Scene/GameScene/mj/SettingLayer.lua
+++ src/app/Scene/GameScene/mj/SettingLayer.lua
@@ -19,7 +19,236 @@ function SettingLayer:ctor()
 	lt.CommonUtil:addNodeClickEvent(dissolveRoom, handler(self, self.onDissolveRoom))
 	lt.CommonUtil:addNodeClickEvent(backLobby, handler(self, self.onBackLobby))
     lt.CommonUtil:addNodeClickEvent(btnClose, handler(self, self.onClose))
+    self:setshow()
+
 end
+
+function SettingLayer:setshow() 
+    local selectNumBgcolor = {}
+    local selectNumMjcolor = {}
+    local selectNumGameyy = {}
+    local selectNumGameyx = {}
+    local selectNumGamelanguage = {}
+    local xuanzhonBgcolor = lt.PreferenceManager:getBgcolor() --记录选中背景颜色
+    local xuanzhonMjcolor = lt.PreferenceManager:getMJcolor() --记录选中麻将颜色
+    local xuanzhonGameyy = lt.PreferenceManager:getGemeyy()   --记录选中游戏音乐
+    local xuanzhonGameyx = lt.PreferenceManager:getGemeyx()   --记录选中游戏音效
+    local xuanzhonGamelanguage = lt.PreferenceManager:getGamelanguage()   --记录选中游戏语言
+    for i=1,3 do    
+        -- select背景颜色
+        local bgPalel = self._scrollView:getChildByName("BGColor_".. i)
+        bgPalel.selectNode = bgPalel:getChildByName("Image_Select")
+        bgPalel._textNode = bgPalel:getChildByName("image_result")
+        selectNumBgcolor[i] = bgPalel
+
+        if xuanzhonBgcolor == 0 then
+         --不存在代表是新手默认在第一项
+            xuanzhonBgcolor = 1
+        end
+
+        if i == xuanzhonBgcolor then
+            bgPalel.selectNode:setVisible(true)
+        else
+            bgPalel.selectNode:setVisible(false)
+        end
+
+        lt.CommonUtil:addNodeClickEvent(bgPalel, function( ... )
+            for i, v in pairs(selectNumBgcolor) do 
+                if v == bgPalel then
+                    v.selectNode:setVisible(true)
+                    lt.PreferenceManager:setBgcolor(i)
+                else
+                    v.selectNode:setVisible(false)
+                end
+            end
+        end, false)
+    end
+
+    for i=1,3 do
+        -- select麻将颜色
+        local mjPalel = self._scrollView:getChildByName("MJColor_".. i)
+        mjPalel.selectNode = mjPalel:getChildByName("Image_Select")
+        mjPalel._textNode = mjPalel:getChildByName("image_result")
+        selectNumMjcolor[i] = mjPalel
+
+        if xuanzhonMjcolor == 0 then
+         --不存在代表是新手默认在第一项
+            xuanzhonMjcolor = 1
+        end
+
+        if i == xuanzhonMjcolor then
+            mjPalel.selectNode:setVisible(true)
+        else
+            mjPalel.selectNode:setVisible(false)
+        end
+
+        lt.CommonUtil:addNodeClickEvent(mjPalel, function( ... )
+            for i, v in pairs(selectNumMjcolor) do 
+                if v == mjPalel then
+                    v.selectNode:setVisible(true)
+                    lt.PreferenceManager:setMJcolor(i)
+                else
+                    v.selectNode:setVisible(false)
+                end
+            end
+        end, false)
+    end
+
+    for i=1,4 do
+        -- select游戏音乐
+        local musicPalel = self._scrollView:getChildByName("BGMusic_".. i)
+        musicPalel.selectNode = musicPalel:getChildByName("Image_Select")
+        musicPalel._textNode = musicPalel:getChildByName("image_result")
+        selectNumGameyy[i] = musicPalel
+
+        if xuanzhonGameyy == 0 then
+         --不存在代表是新手默认在第一项
+            xuanzhonGameyy = 1
+        end
+
+        if i == xuanzhonGameyy then
+            musicPalel.selectNode:setVisible(true)
+        else
+            musicPalel.selectNode:setVisible(false)
+        end
+
+        lt.CommonUtil:addNodeClickEvent(musicPalel, function( ... )
+            for i, v in pairs(selectNumGameyy) do 
+                if v == musicPalel then
+                    v.selectNode:setVisible(true)
+                    lt.PreferenceManager:setGemeyy(i) 
+                else
+                    v.selectNode:setVisible(false)
+                end
+            end
+        end, false)
+    end
+
+    for i=1,2 do
+        -- select游戏音效
+        local yxPalel = self._scrollView:getChildByName("GameMusic_".. i)
+        yxPalel.selectNode = yxPalel:getChildByName("Image_Select")
+        yxPalel._textNode = yxPalel:getChildByName("image_result")
+        selectNumGameyx[i] = yxPalel
+
+        if xuanzhonGameyx == 0 then
+         --不存在代表是新手默认在第一项
+            xuanzhonGameyx = 1
+        end
+
+        if i == xuanzhonGameyx then
+            yxPalel.selectNode:setVisible(true)
+        else
+            yxPalel.selectNode:setVisible(false)
+        end
+
+        lt.CommonUtil:addNodeClickEvent(yxPalel, function( ... )
+            for i, v in pairs(selectNumGameyx) do 
+                if v == yxPalel then
+                    v.selectNode:setVisible(true)
+                    lt.PreferenceManager:setGemeyx(i)  
+                else
+                    v.selectNode:setVisible(false)
+                end
+            end
+        end, false)
+    end
+
+    for i=1,2 do
+        -- select游戏语言
+        local languagePalel = self._scrollView:getChildByName("Language_".. i)
+        languagePalel.selectNode = languagePalel:getChildByName("Image_Select")
+        languagePalel._textNode = languagePalel:getChildByName("image_result")
+        selectNumGamelanguage[i] = languagePalel
+
+        if xuanzhonGamelanguage == 0 then
+         --不存在代表是新手默认在第一项
+            xuanzhonGamelanguage = 1
+        end
+
+        if i == xuanzhonGamelanguage then
+            languagePalel.selectNode:setVisible(true)
+        else
+            languagePalel.selectNode:setVisible(false)
+        end
+
+        lt.CommonUtil:addNodeClickEvent(languagePalel, function( ... )
+            for i, v in pairs(selectNumGamelanguage) do 
+                if v == languagePalel then
+                    v.selectNode:setVisible(true)
+                    lt.PreferenceManager:setGamelanguage(i)  
+                else
+                    v.selectNode:setVisible(false)
+                end
+            end
+        end, false)
+    end
+
+    local Ie_PBVoiceCheck = self._scrollView:getChildByName("Ie_PBVoiceCheck")  --游戏语音
+    local GameVoice = self._scrollView:getChildByName("GameVoice")              --游戏语音select
+    local xuanzhonstartYuyin = lt.PreferenceManager:getGameyuyin()
+    if xuanzhonstartYuyin == 1 then
+        Ie_PBVoiceCheck:setVisible(true)
+    else
+        Ie_PBVoiceCheck:setVisible(false)
+    end
+
+    lt.CommonUtil:addNodeClickEvent(GameVoice, function( ... )  
+        local xuanzhonYuyin = lt.PreferenceManager:getGameyuyin()
+        if xuanzhonYuyin == 1 then --当前如果是1代表目前是已选中状态，
+            Ie_PBVoiceCheck:setVisible(false)
+            lt.PreferenceManager:setGameyuyin(2)
+        elseif xuanzhonYuyin == 2 then
+            Ie_PBVoiceCheck:setVisible(true)
+            lt.PreferenceManager:setGameyuyin(1)
+            
+        end
+    end, false)
+
+    local Ie_PBVibratCheck = self._scrollView:getChildByName("Ie_PBVibratCheck") --震动提醒
+    local GameVibrat = self._scrollView:getChildByName("GameVibrat")             --震动提醒select
+    local xuanzhonstartGameVibrat = lt.PreferenceManager:getGamevibrate()
+    if xuanzhonstartGameVibrat == 1 then
+        Ie_PBVibratCheck:setVisible(true)
+    else
+        Ie_PBVibratCheck:setVisible(false)
+    end
+
+    lt.CommonUtil:addNodeClickEvent(GameVibrat, function( ... )
+        local xuanzhonGameVibrat = lt.PreferenceManager:getGamevibrate()
+        if xuanzhonGameVibrat == 1 then --当前如果是1代表目前是已选中状态，
+            Ie_PBVibratCheck:setVisible(false)
+            lt.PreferenceManager:setGamevibrate(2)
+        elseif xuanzhonGameVibrat == 2 then
+            Ie_PBVibratCheck:setVisible(true)
+            lt.PreferenceManager:setGamevibrate(1)
+            
+        end
+    end, false)
+
+    local Ie_PBShakeCheck = self._scrollView:getChildByName("Ie_PBShakeCheck") --抖动特效
+    local GameShake = self._scrollView:getChildByName("GameShake")              --抖动特效select
+    local xuanzhonstartshake = lt.PreferenceManager:getGameshake()
+    if xuanzhonstartshake == 1 then
+        Ie_PBShakeCheck:setVisible(true)
+    else
+        Ie_PBShakeCheck:setVisible(false)
+    end
+
+    lt.CommonUtil:addNodeClickEvent(GameShake, function( ... )
+        local xuanzhonshake = lt.PreferenceManager:getGameshake()
+        if xuanzhonshake == 1 then --当前如果是1代表目前是已选中状态，
+            Ie_PBShakeCheck:setVisible(false)
+            lt.PreferenceManager:setGameshake(2)
+        elseif xuanzhonshake == 2 then
+            Ie_PBShakeCheck:setVisible(true)
+            lt.PreferenceManager:setGameshake(1)
+            
+        end
+    end, false)
+
+end
+
 
 function SettingLayer:onDissolveRoom()
     
